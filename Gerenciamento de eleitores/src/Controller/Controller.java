60769bf748cde16e7b0ac7d450247e704ac62c8e
cleybson cardoso <cleybsonc@gmail.com>
2017-01-31 15:41:58 -0300

    iniciando

---
+++ Gerenciamento de eleitores/src/Controller/Controller.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author cleyb
+ */
+public class Controller {
+    
+    public String diretorio;
+    
+    public boolean logar(String nome, String password){
+        return false;
+    }
+    
+    public void deslogar(){
+        
+    }
+    
+    public void carregarInformacoes(){
+        
+    }
+    
+    public boolean salvar(){
+        return false;
+    }
+    
+    public boolean cadastrar(){
+        return false;
+    }
+    
+    public boolean editar(){
+        return false;
+    }
+    
+    
+}
---
+++ Gerenciamento de eleitores/src/gerenciamento/de/eleitores/GerenciamentoDeEleitores.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerenciamento.de.eleitores;
+
+import View.Tela;
+
+/**
+ *
+ * @author cleyb
+ */
+public class GerenciamentoDeEleitores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tela tela = new Tela();
+    }
+    
+}
